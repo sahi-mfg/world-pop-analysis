2951e4c9e6fc06fc41bd5f6b7fc459d8946922c4
Sahi Mohamed Francis Gonsangbeu <mohamedfrancissahi@gmail.com>
2024-11-29 10:05:17 +0100

    feat: update data retrieval URL to include specific dataset and geojson parameters

--- data_retrieval.py
+++ data_retrieval.py
@@ -7,7 +7,7 @@ logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(message)s")
 
 
 async def get_data() -> None:
-    url = "https://www.worldpop.org/rest/data/pop/"
+    url = "https://api.worldpop.org/v1/services/stats?dataset=wpgppop&year=2010&geojson={“type”:”FeatureCollection”,”features”:[{“type”:”Feature”,”properties”:{},”geometry”:{“type”:”Polygon”,”coordinates”:[[[10.546875,47.62097541515849],[9.95361328125,46.437856895024204],[11.315917968749998,45.98169518512228],[12.63427734375,46.66451741754235],[12.65625,47.85740289465826],[10.546875,47.62097541515849]]]}}]}"
     data = await http_get(url)
     print(data)
 
