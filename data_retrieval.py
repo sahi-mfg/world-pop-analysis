eec9f5d8b797590c2df5baf02e8d55696d6f4ff0
Sahi Mohamed Francis Gonsangbeu <mohamedfrancissahi@gmail.com>
2024-12-17 12:59:45 +0100

    feat: modify get_data function to accept a URL parameter for flexibility

--- data_retrieval.py
+++ data_retrieval.py
@@ -6,11 +6,11 @@ from req_http import http_get
 logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(message)s")
 
 
-async def get_data() -> None:
-    url = "https://jsonplaceholder.typicode.com/posts/1"
+async def get_data(url: str) -> None:
     data = await http_get(url)
     print(data)
 
 
 if __name__ == "__main__":
-    asyncio.run(get_data())
+    url = "https://jsonplaceholder.typicode.com/posts/1"
+    asyncio.run(get_data(url))
